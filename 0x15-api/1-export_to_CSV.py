b7aac5aec09c35e13d1eb7784a46b2b66e28fb40
ENuel21 <emmaonana18@gmail.com>
2024-08-12 06:32:01 +0100

    api

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""This script uses REST API, accepts an employee ID as an argument and
+retuns inforation about his/her TODO progress
+"""
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    id = argv[1]
+    url1 = f'https://jsonplaceholder.typicode.com/users/{id}'
+    url2 = f'https://jsonplaceholder.typicode.com/todos?userId={id}'
+
+    resp = requests.get(url1)
+    name = resp.json().get("username")
+
+    res = requests.get(url2)
+    res = res.json()
+    if name:
+        lists = []
+        fields = ['id', 'username', 'completed', 'title']
+        filename = f'{id}.csv'
+        csvfile = open(filename, 'w')
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fields,
+                                   quoting=csv.QUOTE_ALL)
+
+        for user in res:
+            doneOrNot = user.get('completed')
+            title = user.get('title')
+
+            row = {'id': id, 'username': name, 'completed': doneOrNot,
+                   'title': title}
+            lists.append(row)
+        csvwriter.writerows(lists)
